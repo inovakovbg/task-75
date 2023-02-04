d74ae7d830cbdc78d672d5a727363df49bece01a
Ivan Novakov <inovakov.bg@gmail.com>
2023-02-04 14:57:02 +0200

    Add
    probably solved

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import "./App.css";
+import React ,{useEffect, useState} from "react";
 
 function App() {
+
+
+
+  const [text , setText] = useState(JSON.parse(localStorage.getItem('dataText')));
+
+  console.log({text});
+
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea 
+               value={text}
+               className="textarea is-large" 
+               placeholder="Notes..."
+               onChange={(e) => setText(e.target.value)} />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button className="button is-large is-primary is-active"
+        onClick={(e) => localStorage.setItem('dataText', JSON.stringify(text))}
+        >Save
+        </button>
+
+        <button 
+        className="button is-large"
+        onClick={(e) =>{setText("")}}
+        >Clear
+        </button>
       </div>
     </div>
   );
